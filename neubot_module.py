9544074aa5012f4e47ec7f432532ce854f408b11
DavideAllavena <davide.allavena@gmail.com>
2015-11-10 12:04:50 +0100

    Added negotiate files.

---
+++ neubot_module.py
@@ -0,0 +1,48 @@
+# mod_raw_test/neubot_module.py
+
+#
+# Copyright (c) 2013
+#     Nexa Center for Internet & Society, Politecnico di Torino (DAUIN)
+#     and Simone Basso <bassosimone@gmail.com>
+#
+# This file is part of Neubot <http://www.neubot.org/>.
+#
+# Neubot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Neubot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+""" The entry point of the Raw Test module """
+
+import logging
+from ..lib_net.poller import POLLER
+from ..mod_raw_test.raw_srvr_glue import RAW_SERVER_EX
+def _run_test(message):
+    """ Run the Raw Test test """
+    raise RuntimeError("Not implemented")
+
+def mod_load(context, message):
+    """ Invoked when the module loads """
+    logging.debug("raw_test: init for context '%s'... in progress", context)
+
+    if context == "server":
+
+        negotiate_server = message["negotiate_server"]
+        http_server = message["http_server"]
+
+        logging.debug("raw_test: register negotiate server module... in progress")
+
+        conf = message["configuration"]
+        RAW_SERVER_EX.listen((conf["address"], 12345), conf['prefer_ipv6'], 0, '')
+        logging.debug('server: starting raw server... complete')
+
+    logging.debug("raw_test: init for context '%s'... complete", context)
